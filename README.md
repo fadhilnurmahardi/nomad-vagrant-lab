cfaba5eb4c10ea84ee553318c31a498cc1f3353e
Fadhil Nur Mahardi <fadhilnurmahardi49@gmail.com>
2021-05-25 16:23:32 +0700

    fix(launch-script): for customizable run vault and init vault

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # HashiCorp Nomad - Local Lab Using Vagrant
 ## What is this?
 
-A simple 3-node or 6-node lab running Ubuntu servers on VirtualBox and each node runs Consul and Nomad servers which can be configured as a cluster.
+A simple 3-node or 6-node lab running Ubuntu servers on VirtualBox and each node runs Consul and Nomad servers which can be configured as a cluster. And able to run vault to one node (currently cannot be clustered because use file storage)
 
 ## Why use this method?
 
@@ -20,21 +20,23 @@ There are a few things you need to get this going:
 ### For 3-node clusters you must rename `Vagrantfile.3node` to `Vagrantfile`
 ### For 6-node (two region) clusters you must rename `Vagrantfile.6node` to `Vagrantfile`
 
-* Clone this repo (or fork it of you so desire and want to contribute to it)
-
 * Change directory and run a `vagrant status` to check the dependencies are met
 
-* Run a `vagrant up` command and watch the magic happen! (spoiler alert: it's not magic, it's technology)
+* Run a `vagrant up` command and watch the magic happen!
 
-* Each node will able to run Consul and Nomad 
+* Each node will able to run Consul, Nomad and Vault (with file storage)
 
 To start your Nomad cluster just do this: 
 
-* `vagrant ssh <node_name> -c "sudo /vagrant/launch-nomad-consul.sh"` where `<nodename>` is the instance name (e.g. nomad-a-1, nomad-b-1).
+* `vagrant ssh <node_name> -c "sudo /vagrant/launch-services.sh"` where `<nodename>` is the instance name (e.g. nomad-a-1, nomad-b-1).
+
+* To run vault & init on nomad-a-1 node `vagrant ssh nomad-a-1 -c "sudo /vagrant/launch-services.sh --run-vault --init-vault"`
+
+* The initial vault secret will be on your `/vagrant/playgroud/vault` (inside vagrant instance) or `./playgroud/vault` (outside vagrant instance)
 
 Now you're running!
 
-## Interacting with the Nomad and Consul cluster
+## Interacting with the cluster
 
 Logging into the systems locally can be done 
 
@@ -45,5 +47,13 @@ nomad node status
 * To open the Nomad UI use this command on your local machine
 ```
 open http://172.16.1.101:4646
+```
+* To open the Consul UI use this command on your local machine
+```
+open http://172.16.1.101:8500
+```
+* To open the Vault UI use this command on your local machine
+```
+open http://172.16.1.101:8200
 ```
 
--- launch-services.sh
+++ launch-services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 function generate_systemd_config_consul {
   local systemd_config_path="/etc/systemd/system/consul.service"
   echo "Creating systemd config file to run Consul in $systemd_config_path"
@@ -49,19 +51,24 @@ Description="HashiCorp Nomad"
 Documentation=https://www.nomadproject.io/
 Requires=network-online.target
 After=network-online.target
-ConditionalFileNotEmpty=$config_path
 EOF
 )
 
   local readonly service_config=$(cat <<EOF
 [Service]
-User=vagrant
-Group=vagrant
+User=root
+Group=root
 ExecStart=/usr/bin/nomad agent -config /etc/nomad.d/
 ExecReload=/bin/kill --signal HUP \$MAINPID
 KillMode=process
+KillSignal=SIGINT
+LimitNOFILE=infinity
+LimitNPROC=infinity
 Restart=on-failure
-LimitNOFILE=65536
+RestartSec=2
+StartLimitBurst=3
+StartLimitIntervalSec=10
+TasksMax=infinity
 EOF
 )
 
@@ -81,7 +88,6 @@ function generate_systemd_config_vault {
   local vault_config_dir="/etc/vault.d/"
   local config_path="$config_dir/default.hcl"
 
-  local vault_description="HashiCorp Vault - A tool for managing secrets"
   local vault_command="server"
   local vault_config_file_or_dir="$vault_config_dir"
 
@@ -89,7 +95,7 @@ function generate_systemd_config_vault {
 
   local -r unit_config=$(cat <<EOF
 [Unit]
-Description=\"$vault_description\"
+Description="HashiCorp Vault - A tool for managing secrets"
 Documentation=https://www.vaultproject.io/docs/
 Requires=network-online.target
 After=network-online.target
@@ -137,20 +143,89 @@ EOF
   echo -e "$install_config" >> "$systemd_config_path"
 }
 
-generate_systemd_config_consul
-generate_systemd_config_nomad
-generate_systemd_config_vault
+function run_consul_service {
+  sudo systemctl enable consul
+  sudo setcap cap_net_bind_service=+ep /usr/bin/consul
+  sudo systemctl restart consul
+  PRIVATE_IP=$(hostname -I | awk '{print $2}')
+  sudo sed -i "s/#DNS=/DNS=$PRIVATE_IP/g" "/etc/systemd/resolved.conf"
+  sudo sed -i "s/#DNS=/DNS=$PRIVATE_IP/g" "/etc/systemd/resolved.conf"
+  sudo systemctl restart systemd-resolved.service
+}
 
-systemctl enable consul
-systemctl enable vault
-systemctl enable nomad
-sudo setcap cap_net_bind_service=+ep /usr/bin/consul
-systemctl restart consul
-PRIVATE_IP=$(hostname -I | awk '{print $2}')
-sudo sed -i "s/#DNS=/DNS=$PRIVATE_IP/g" "/etc/systemd/resolved.conf"
-sudo sed -i "s/#DNS=/DNS=$PRIVATE_IP/g" "/etc/systemd/resolved.conf"
-sudo sed -i "s/##IP_SERVER##/$PRIVATE_IP/g" "/etc/vault.d/default.hcl"
-sudo sed -i "s/##IP_SERVER##/$PRIVATE_IP/g" "/etc/nomad.d/default.hcl"
-sudo systemctl restart systemd-resolved.service
-systemctl restart vault
-systemctl restart nomad
+function run_nomad_service {
+  PRIVATE_IP=$(hostname -I | awk '{print $2}')
+  sudo sed -i "s/##IP_SERVER##/$PRIVATE_IP/g" "/etc/nomad.d/default.hcl"
+  sudo systemctl enable nomad
+  sudo systemctl restart nomad
+}
+
+function run_vault_service {
+  mkdir -p /data
+  chown vagrant /data
+  PRIVATE_IP=$(hostname -I | awk '{print $2}')
+  sudo sed -i "s/##IP_SERVER##/$PRIVATE_IP/g" "/etc/vault.d/default.hcl"
+  sudo systemctl enable vault
+  sudo systemctl restart vault
+}
+
+function run_init_vault {
+  echo "Start init vault"
+  export VAULT_ADDR=http://127.0.0.1:8200
+  vault_secrets="$(vault operator init -key-shares=5 -key-threshold=5 || echo "false")"
+
+  if [[ "$vault_secrets" != "false" ]]; then
+    mkdir -p /vagrant/playgroud
+    sudo tee /vagrant/playgroud/vault>/dev/null<<EOF
+"$vault_secrets"
+EOF
+  fi
+
+  ITER=0
+  IFS=$'\n'
+  for key in $(cat /vagrant/playgroud/vault | grep -o ': .*' | sed 's@: @@'); do
+      if [[ $ITER -eq 5 ]]; then
+          break
+      else 
+          vault operator unseal $key
+      fi
+      ITER=$(expr $ITER + 1)
+  done
+  echo "Vault unsealed"
+}
+
+function run {
+  local run_vault="false"
+  local init_vault="false"
+
+  while [[ $# > 0 ]]; do
+    local key="$1"
+
+    case "$key" in
+      --run-vault)
+        run_vault="true"
+        ;;
+      --init-vault)
+        init_vault="true"
+        ;;
+    esac
+
+    shift
+  done
+
+  generate_systemd_config_consul
+  generate_systemd_config_nomad
+  generate_systemd_config_vault
+
+  run_consul_service
+  if [[ "$run_vault" == "true" ]]; then
+    run_vault_service
+    if [[ "$init_vault" == "true" ]]; then
+      sleep 5
+      run_init_vault
+    fi
+  fi
+  run_nomad_service
+}
+
+run "$@"
